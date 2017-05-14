f757246b3a9660d330db75f0e7665414d9b15df4
ethanryan <ethanryan@gmail.com>
2017-05-14 19:14:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 //Code React element here
+
+const meInReact =
+  React.createElement('div', { className: 'me' },
+    [
+      React.createElement('h1', { className: 'Title'}, 'An Awesome Person'),
+      React.createElement('p', {}, 'Who is learning React'),
+      React.createElement('ul', { className: 'interests'},
+        [
+            React.createElement('li', {}, 'JavaScript'),
+            React.createElement('li', {}, 'React'),
+            React.createElement('li', {}, 'Movies'),
+            React.createElement('li', {}, 'Ice cream')
+        ]), //ends four children of 'interests'
+    ] //ends three children of 'me'
+  ); //ends className: 'me'
+
+ReactDOM.render(meInReact, document.getElementById('main'));
